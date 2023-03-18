96c6287d3e786bc172e6e1f9a071f4d35d4775a9
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-03-18 21:07:48 +0100

    Fix: redisdb client and set structure for other features

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import appController from '../controller/AppController';
 import auth from '../controller/AuthController';
+import inventoryController from '../controller/InventoryController';
+import roleController from '../controller/RoleController';
+import userController from '../controller/UserController';
 
 const router = express.Router();
 
@@ -8,12 +11,16 @@ router.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-router.get('/status', (req, res) => {
-    appController.getStatus(req, res)
-})
+// router.get('/status', (req, res) => {
+//     appController.getStatus(req, res)
+// })
 
-router.get('/login', (req, res) => {
+router.get('/status', appController.getStatus)
+
+router.post('/login', (req, res) => {
     auth.login(req, res);
 })
+
+// router.get("/inventory/:id", auth.auth, inventoryController.get)
 
 export default router;
--- src/utils/redis.server.ts
+++ src/utils/redis.server.ts
@@ -1,135 +1,39 @@
-const redis = require('redis');
-/**
- * basic usage of creating a connection in redis 
- * const redisClient = redis.createClient({url: process.env.REDIS_URL || 'redis://localhost:6379'});
-  async ({ async }: { async: boolean }) => await redisClient.connect();
-console.log("client connection is successful")
-async ({ async }: { async: boolean }) => {
-    redisClient.on('error', (err: any) => console.log('Redis Client Error', err));
-    await redisClient.connect();
-};
-*/
-
+import { promisify } from 'util';
+// import { createClient } from 'redis';
+const redis = require('redis')
 
 class RedisClient {
-  redisClient: any;
+  private client: any;
+  private isClientConnected: boolean;
 
   constructor() {
-    this.redisClient = redis.createClient({
-        host: 'localhost',
-        port: 6379
+    this.client = redis.createClient({url: process.env.REDIS_URL || 'redis://localhost:6379'});
+    this.isClientConnected = false
+    this.client.on('error', (err: Error) => {
+      console.error('Redis client failed to connect:', err.message || err.toString());
+      this.isClientConnected = false;
     });
-
-    this.redisClient.on('connect', () => {
-      console.log('Redis client connected');
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
     });
-
-    this.redisClient.on('error', (err:any) => {
-      console.error('Error connecting to Redis:', err);
-    });
-    
   }
-  // checks if the connection is alive
+
   isAlive(): boolean {
-    return this.redisClient.connected;
-  };
-
-  async getRedisValue(key: string): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-      this.redisClient.get(key, (err:any, reply:any) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(reply);
-        }
-      });
-    });
+    return this.isClientConnected;
   }
 
-  async set(key: string, value: string, expireSeconds: number): Promise<void> {
-    await new Promise<void>((resolve, reject) => {
-      this.redisClient.setex(key, expireSeconds, value, (err: any) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    });
+  async get(key: string): Promise<string | null> {
+    return promisify(this.client.GET).bind(this.client)(key);
   }
-  
-  async del(key: string): Promise<number> {
-    return new Promise<number>((resolve, reject) => {
-      this.redisClient.del(key, (err: Error | null, reply: number) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(reply);
-        }
-      });
-    });
+
+  async set(key: string, value: string | number | boolean, duration: number): Promise<void> {      
+    await promisify(this.client.SETEX).bind(this.client)(key, duration, String(value));
   }
-  
 
-  };
-  
-
-
-// await client.connect();
-// check if the connection is working}
-// class RedisClient {
-//   constructor() {
-//     this.client = createClient();
-//     // this.client = redis.createClient();
-
-//     this.client.on('error', (err) => console.error('Error occurred while connecting to Redis:', err));
-//   }
-
-//   isAlive() {
-//     // return this.client && this.client.readyState === "ready";
-//     return this.client.connected;
-//   }
-
-//   async get(key) {
-//     return new Promise((resolve, reject) => {
-//       this.client.get(key, (err, value) => {
-//         if (err) {
-//           reject(err);
-//         } else {
-//           resolve(value);
-//         }
-//       });
-//     });
-//   }
-
-//   async set(key, value, duration) {
-//     return new Promise((resolve, reject) => {
-//       this.client.set(key, value, 'EX', duration, (err) => {
-//         if (err) {
-//           reject(err);
-//         } else {
-//           resolve();
-//         }
-//       });
-//     });
-//   }
-
-//   async del(key) {
-//     return new Promise((resolve, reject) => {
-//       this.client.del(key, (err) => {
-//         if (err) {
-//           reject(err);
-//         } else {
-//           resolve();
-//         }
-//       });
-//     });
-//   }
-// }
-const redisClient = new RedisClient();
-console.log(`${redisClient.isAlive()}`)
-export { redis, redisClient };
-
-function async() {
-    throw new Error("Function not implemented.");
+  async del(key: string): Promise<void> {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
 }
+
+export const redisClient = new RedisClient();
+export default redisClient;
