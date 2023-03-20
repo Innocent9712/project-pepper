0ca07b322a1d47780776808d57fdca38f3a5e402
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-03-20 18:02:41 +0100

    Setup linting

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,11 +8,9 @@ import userController from '../controller/UserController';
 
 const router = express.Router();
 
-
-
 // Start
 router.get('/', (req, res) => {
-    res.send('Hello and welcome to the Pepper Inventory!');
+  res.send('Hello and welcome to the Pepper Inventory!');
 });
 // curl localhost:5000/api/v1; echo ""
 
@@ -28,48 +26,47 @@ router.get('/', (req, res) => {
  *          200:
  *              description: DBs are active
  */
-router.get('/status', appController.getStatus)
+router.get('/status', appController.getStatus);
 // curl localhost:5000/api/v1/status; echo ""
 
 // Login
-router.post('/login', auth.login)
+router.post('/login', auth.login);
 // curl -i -XPOST localhost:5000/api/v1/login -H "Authorization: Basic c3VwZXJhZG1pbjpzdXBlcmFkbWlu"; echo ""
 
 // Logout
-router.post('/logout', auth.logout)
+router.post('/logout', auth.logout);
 // curl -XPOST -b "token=87772534-462e-4a18-aba7-5e15e6f4a5c7" localhost:5000/api/v1/logout; echo ""
 
 // router.get("/inventory/:id", auth.auth, inventoryController.get)
 
 // Get all roles
-router.get('/roles', auth.auth, roleController.getAll)
+router.get('/roles', auth.auth, roleController.getAll);
 // curl -b "token=ba52ccff-ff97-4995-9366-0af0cbf97dcb" localhost:5000/api/v1/roles; echo ""
 
-
 // Get a single role
-router.get('/roles/:roleID', auth.auth, roleController.getRole)
+router.get('/roles/:roleID', auth.auth, roleController.getRole);
 // curl -b "token=ba52ccff-ff97-4995-9366-0af0cbf97dcb" localhost:5000/api/v1/roles/1; echo ""
 // Create a new role
-router.post('/roles', auth.auth, roleController.createRole)
+router.post('/roles', auth.auth, roleController.createRole);
 // curl -XPOST -b "token=ba52ccff-ff97-4995-9366-0af0cbf97dcb" localhost:5000/api/v1/roles -H "Content-Type: application/json" -d '{"name": "temp"}'; echo ""
 
 // Delete a role
-router.delete('/roles/:roleID', auth.auth, roleController.deleteRole)
+router.delete('/roles/:roleID', auth.auth, roleController.deleteRole);
 
 // Update a role
-router.put('/roles/:roleID', auth.auth, roleController.updateRole)
+router.put('/roles/:roleID', auth.auth, roleController.updateRole);
 // curl -XPUT -b "token=ba52ccff-ff97-4995-9366-0af0cbf97dcb" localhost:5000/api/v1/roles/2 -H "Content-Type: application/json" -d '{"name": "temp-rename"}'; echo ""
 
-router.get('/roles/:roleID/permissions', auth.auth, roleController.getRolePermissions)
+router.get('/roles/:roleID/permissions', auth.auth, roleController.getRolePermissions);
 // Add permission to a role
-router.post('/roles/:roleID/add-permission', auth.auth, roleController.addPermission)
+router.post('/roles/:roleID/add-permission', auth.auth, roleController.addPermission);
 // curl -XPOST -b "token=ba52ccff-ff97-4995-9366-0af0cbf97dcb" localhost:5000/api/v1/roles/add-permission?roleID=2&permissionID=3; echo ""
 
 // Remove permission from a role
-router.delete('/roles/:roleID/remove-permission', auth.auth, roleController.removePermission)
+router.delete('/roles/:roleID/remove-permission', auth.auth, roleController.removePermission);
 
 // Get all permissions
-router.get('/permissions', auth.auth, permissionController.getAll)
+router.get('/permissions', auth.auth, permissionController.getAll);
 // curl -b "token=ba52ccff-ff97-4995-9366-0af0cbf97dcb" localhost:5000/api/v1/permissions; echo ""
 
 export default router;
