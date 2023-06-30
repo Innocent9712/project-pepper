a87455b5a31964fd5cc3c91ac4359bec3f623ab6
Mustapha Awwal <awwalmustapha41@gmail.com>
2023-06-30 16:54:35 +0000

    done

---
+++ codepipeline.tf
@@ -0,0 +1,103 @@
+resource "aws_codepipeline" "codepipeline" {
+  name     = "capstone-pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+  artifact_store {
+    location = aws_s3_bucket.capstone.bucket
+    type     = "S3"
+  }
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        OAuthToken           = var.github_token
+        Owner                = "Innocent9712"
+        Repo                 = "project-pepper"
+        Branch               = "CICD"
+        PollForSourceChanges = true
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      version          = "1"
+
+      configuration = {
+        ProjectName = aws_codebuild_project.capstone_build.name
+
+      }
+    }
+  }
+
+}
+
+data "aws_iam_policy_document" "pipeline_role_doc" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name               = "capstone-role"
+  assume_role_policy = data.aws_iam_policy_document.pipeline_role_doc.json
+}
+
+data "aws_iam_policy_document" "codepipeline_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+      "s3:PutObjectAcl",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.capstone.arn}",
+      "${aws_s3_bucket.capstone.arn}/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name   = "codepipeline_policy"
+  role   = aws_iam_role.codepipeline_role.id
+  policy = data.aws_iam_policy_document.codepipeline_policy.json
+}
+
