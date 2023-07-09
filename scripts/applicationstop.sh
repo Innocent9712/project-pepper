c31ff912c69e25cdc23e6dfcdf2b327ea070e24f
Mustapha Awwal <awwalmustapha41@gmail.com>
2023-07-09 03:18:43 +0100

    Update applicationstop.sh

--- scripts/applicationstop.sh
+++ scripts/applicationstop.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+# #!/bin/bash
 
-SERVICE_FILE="/etc/systemd/system/project-pepper.service"
+# SERVICE_FILE="/etc/systemd/system/project-pepper.service"
 
-# Check if the service file exists
-if [ -f "$SERVICE_FILE" ]; then
-  # Stop the service
-  systemctl stop project-pepper.service
-else
-  echo "Service file not found. Skipping service stop."
-fi
+# # Check if the service file exists
+# if [ -f "$SERVICE_FILE" ]; then
+#   # Stop the service
+#   systemctl stop project-pepper.service
+# else
+#   echo "Service file not found. Skipping service stop."
+# fi
