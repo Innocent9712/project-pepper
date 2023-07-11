c6eab6a85a29e4a1456169304ce9ca93e7272067
Mustapha Awwal <awwalmustapha41@gmail.com>
2023-07-11 18:00:05 +0100

    Update start_backend.sh

--- scripts/start_backend.sh
+++ scripts/start_backend.sh
@@ -3,11 +3,11 @@ source ../env
 export DATABASE_URL=$DATABASE_URL
 export HONEYCOMB_API_KEY=$HONEYCOMB_API_KEY
 
-#sudo cp project-pepper.service /etc/systemd/system/
+sudo cp project-pepper.service /etc/systemd/system/
 cd ../code/backend
-#sudo npm i
+sudo npm i
 sudo ./migrate-seed-build.sh $DATABASE_URL
 npm run start-instrumented
 #HONEYCOMB_API_KEY=(echo $HONEYCOMB_API_KEY) OTEL_SERVICE_NAME=(echo $OTEL_SERVICE_NAME) npm run start-instrumented
-#sudo systemctl start project-pepper
-#sudo systemctl enable project-pepper
+sudo systemctl start project-pepper
+sudo systemctl enable project-pepper
