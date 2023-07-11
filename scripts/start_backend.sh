21a22d86d2b311c406d4dc116c076205ca17acef
Mustapha Awwal <awwalmustapha41@gmail.com>
2023-07-11 17:12:47 +0000

    done

--- scripts/start_backend.sh
+++ scripts/start_backend.sh
@@ -7,7 +7,7 @@ sudo cp project-pepper.service /etc/systemd/system/
 cd ../code/backend
 sudo npm i
 sudo ./migrate-seed-build.sh $DATABASE_URL
-npm run start-instrumented
+#npm run start-instrumented
 #HONEYCOMB_API_KEY=(echo $HONEYCOMB_API_KEY) OTEL_SERVICE_NAME=(echo $OTEL_SERVICE_NAME) npm run start-instrumented
 sudo systemctl start project-pepper
 sudo systemctl enable project-pepper
